d88e0212c4beec4802ab85dcd65e2a6ef7c91241
Nicholas Bekeris <nick@illustrious.cloud>
2024-06-19 22:37:30 -0500

    ci: Test with background docker image

--- src/utils/run-tests.sh
+++ src/utils/run-tests.sh
@@ -5,7 +5,10 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 $DIR/reset-db.sh
 bun run db:docker &
 sleep 2
-$DIR/wait-for-it.sh localhost:5432 --strict --timeout=300
+./wait-for-it.sh localhost:5432 --strict --timeout=300
+# Cheeky for pg reboot (?)
+sleep 5
+./wait-for-it.sh localhost:5432 --strict --timeout=300
 bun run db:migrate
 bun test
 $DIR/reset-db.sh

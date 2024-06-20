837611dcfebf2970edea8ebeb6dc33ad2ab943c4
Nicholas Bekeris <nick@illustrious.cloud>
2024-06-19 19:26:39 -0500

    test(ci): Updates for ci testing

--- src/utils/run-tests.sh
+++ src/utils/run-tests.sh
@@ -2,19 +2,9 @@
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
-if [ -z "$1" ]; then
-  echo "Executing locally, use docker image"
-  $DIR/reset-db.sh
-  bun run db:docker &
-else
-  echo "Executing in CI, do NOT use docker image"
-fi
+$DIR/reset-db.sh
+bun run db:docker &
 sleep 4
 bun run db:migrate
 bun test
-if [ -z "$1" ]; then
-  echo "Executing locally, reset database docker instance"
-  $DIR/reset-db.sh
-else
-  echo "Executing in CI, pass through"
-fi
+$DIR/reset-db.sh

eb70c7a77c850e5ba135c7fc8f80dd9293c21b83
Nicholas Bekeris <nick@illustrious.cloud>
2024-06-19 22:29:18 -0500

    ci: Testing configuration with local db

--- src/utils/run-tests.sh
+++ src/utils/run-tests.sh
@@ -4,7 +4,8 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 
 $DIR/reset-db.sh
 bun run db:docker &
-sleep 4
+sleep 2
+$DIR/wait-for-it.sh localhost:5432 --strict --timeout=300
 bun run db:migrate
 bun test
 $DIR/reset-db.sh

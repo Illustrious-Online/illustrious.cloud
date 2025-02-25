299b5fb9effcf60de80fe021330ce58d54f2a670
Nicholas Bekeris <nicholas@bekeris.me>
2025-02-24 20:49:09 -0600

    fix: Change default ports to 3000 everywhere

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,12 +3,11 @@ import data from "../../package.json";
 export default {
   app: {
     env: Bun.env.NODE_ENV || "development",
-    url: Bun.env.APP_URL || "http://localhost:8000",
+    url: Bun.env.APP_URL || "http://localhost:3000",
     name: data.name,
     version: data.version,
     host: Bun.env.APP_HOST || "localhost",
-    port: Bun.env.APP_PORT || "8000",
-    dashboardUrl: Bun.env.DASHBOARD_URL,
+    port: Bun.env.APP_PORT || "3000",
     sentryUrl: Bun.env.SENTRY_URL,
   },
   auth: {
